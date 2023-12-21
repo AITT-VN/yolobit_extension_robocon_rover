4ee67fcfd858656b9ae4bf5cfe1dd18449a43e43
Minh <vanminh0910@gmail.com>
2023-12-21 19:57:31 +0700

    Add remote control mode

---
+++ remote_control.py
@@ -0,0 +1,115 @@
+import time
+from micropython import const
+from rover import *
+from yolobit import *
+from machine import Pin, SoftI2C
+from utility import *
+import gamepad
+
+BTN_FORWARD = '!B516'
+BTN_BACKWARD = '!B615'
+BTN_LEFT = '!B714'
+BTN_RIGHT = '!B814'
+
+BTN_A = '!B11:'
+BTN_B = '!B219'
+BTN_C = '!B318'
+BTN_D = '!B417'
+
+
+class RemoteControlMode():
+
+    def __init__(self):
+        self._speed = 50
+        self._cmd = None
+        self._last_cmd = None
+
+        self._cmd_handlers = {
+            BTN_A: None,
+            BTN_B: None,
+            BTN_C: None,
+            BTN_D: None,
+        }
+
+        try:
+            self._i2c_gp = SoftI2C(scl=Pin(22), sda=Pin(21))
+            self._gamepad_v2 = gamepad.GamePadReceiver(self._i2c_gp)
+        except:
+            self._gamepad_v2 = None
+            print('Gamepad V2 Receiver not found')
+        
+        ble.on_receive_msg('string', self.on_ble_cmd_received)
+
+    def on_ble_cmd_received(self, cmd):
+        print('New command: ', cmd)
+        self._cmd = cmd
+    
+    def set_command(self, cmd, handler):
+        if cmd not in self._cmd_handlers:
+            print('Invalid remote control command')
+            return
+
+        self._cmd_handlers[cmd] = handler
+
+    def run(self):
+        # read command from gamepad v2 receiver if connected
+        if self._gamepad_v2:
+            # read status
+            pass
+
+        if self._cmd != self._last_cmd: # got new command
+            self._speed = 20 # reset speed
+        else:
+            if self._speed < 50:
+                self._speed = self._speed + 1
+            else:
+                self._speed = 50
+
+        if self._cmd == BTN_FORWARD:
+            rover.forward(self._speed*2)
+
+        elif self._cmd == BTN_BACKWARD:
+            rover.backward(self._speed*2)
+
+        elif self._cmd == BTN_LEFT:
+            rover.turn_left(self._speed)
+
+        elif self._cmd == BTN_RIGHT:
+            rover.turn_right(self._speed)
+        
+        elif self._cmd in self._cmd_handlers:
+            if self._cmd_handlers[self._cmd] != None:
+                self._cmd_handlers[self._cmd]()
+        
+        else:
+            rover.stop()
+        
+        self._last_cmd = self._cmd
+
+rc_mode = RemoteControlMode()
+
+''' 
+
+# Example code
+
+def on_button_A():
+    # button A: lift down and release gripper
+    rover.servo_write(2, 0)
+    time.sleep_ms(500)
+    rover.servo_write(1, 0)
+
+def on_button_D():
+    # button D: collect and lift up gripper
+    rover.servo_write(1, 90)
+    time.sleep_ms(500)
+    rover.servo_write(2, 90)
+
+# allow user to config what to do when a gamepad button pressed
+rc_mode.set_command(BTN_A, on_button_A)
+rc_mode.set_command(BTN_D, on_button_D)
+
+while True:
+    rc_mode.run()
+    time.sleep_ms(50)
+
+'''
